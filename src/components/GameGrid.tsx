29cafc4c9aeeed535796f7f71d609f6af8f58c9b
abrsh6266 <abrsh6266@gmail.com>
2023-11-07 02:06:32 +0900

    Highlight the selelected genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,6 +7,7 @@ import { Genre } from "../hooks/useGenres";
 
 interface Props{
   selectedGenre: Genre| null
+  
 }
 
 function GameGrid({selectedGenre}: Props) {
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props{
   onSelectGenre:(genre: Genre) => void
+  selectedGenre: Genre | null
 }
 
-function GenreList({onSelectGenre}:Props) {
+function GenreList({selectedGenre,onSelectGenre}:Props) {
   const { data, isLoading, error } = useGenres();
   if(error) return null
   if (isLoading) return <Spinner />;
@@ -20,7 +21,7 @@ function GenreList({onSelectGenre}:Props) {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button onClick={()=>onSelectGenre(genre)} fontSize="lg" variant='link'>{genre.name}</Button>
+            <Button fontWeight={genre.id===selectedGenre?.id?'bold': 'normal'} onClick={()=>onSelectGenre(genre)} fontSize="lg" variant='link'>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
