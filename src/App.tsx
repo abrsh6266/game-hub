afce583c44e535a3afc8e19f192e6c2a50dd2536
abrsh6266 <abrsh6266@gmail.com>
2023-11-07 16:21:11 +0900

    Refactor: Extract query object

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,14 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
+
 function App() {
-  const [selectedGenre,setSelectedGenre] = useState<Genre | null>(null)
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return (
     <Grid
       templateAreas={{
@@ -17,23 +22,31 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
       templateColumns={{
-        base:'1fr',
-        lg:'200px 1fr'
+        base: "1fr",
+        lg: "200px 1fr",
       }}
     >
-      
-        <GridItem area="nav">
-          <NavBar />
-        </GridItem>
-        <Show above="lg">
-          
-      <GridItem area="aside" paddingX={5}>
-        <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-        </Show>
-      <GridItem area="main" >
-        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform)=> setSelectedPlatform(platform)}/>
-        <GameGrid selectedPlatform = {selectedPlatform} selectedGenre={selectedGenre}/>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList
+            selectedGenre={gameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+          />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <PlatformSelector
+          selectedPlatform={gameQuery.platform}
+          onSelectPlatform={(platform) =>
+            setGameQuery({ ...gameQuery, platform })
+          }
+        />
+        <GameGrid
+          gameQuery={gameQuery}
+        />
       </GridItem>
     </Grid>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,14 +4,14 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import useGames, { Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
 interface Props{
-  selectedGenre: Genre| null
-  selectedPlatform: Platform | null
+  gameQuery: GameQuery;
 }
 
-function GameGrid({selectedGenre,selectedPlatform}: Props) {
-  const { data, error, isLoading } = useGames(selectedGenre,selectedPlatform);
+function GameGrid({gameQuery}: Props) {
+  const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
